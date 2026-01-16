1cd4f92cbdccae8342a3cd16277484d503a1ce50
harsh <harshkuvarvanshi@gmail.cm>
2026-01-16 15:55:56 +0530

    Test automatic pipeline trigger

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Deployed-50000 automatically using Jenkins CI/CD!"
+    return "🚀 Jenkins AUTO pipeline is working!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
